1676ec03ab4733838cb5156a57edc3252fade744
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-08-24 08:07:22 +0300

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES
+(1,"Mr. Chocolate",2,"M","white","calm",0),
+(2,"Rowdy",1,"F","dark brown","calm",0),
+(3,"Tabitha",5,"M","black","calm",0),
+(4,"Sergent Brown",7,"F","white","calm",0),
+(5,"Melissa",10,"M","brown","calm",0),
+(6,"Grinch",9,"F","white","calm",0),
+(7,"Wendy",4,"M","fade black","calm",0),
+(8,null,1,"F","white","calm",0); 
